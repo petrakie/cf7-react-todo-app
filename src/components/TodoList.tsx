1cbbb953d6be724e24fe31c9e179f99f39746252
Evangelia Petraki <petrakie@icloud.com>
2025-05-28 17:44:20 +0200

    refactor(types): move todo types and action union in types.ts

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,30 @@
+import { Trash2 } from "lucide-react";
+import type { TodoListProps} from "../types.ts";
+
+
+const TodoList = ({todos, dispatch}: TodoListProps) =>{
+
+    const handleDelete = (id: number) => () => {
+        dispatch({type: "DELETE", payload: id});
+    }
+
+    return (
+        <>
+            <ul className="space-y-2">
+                {todos.map(todo => (
+                    <li key={todo.id} className="flex items-center justify-between bg-gray-100 p-2 rounded">
+                        <span>{todo.text}</span>
+                        <button
+                            onClick={handleDelete(todo.id)}
+                            className="text-cf-dark-red"
+                        >
+                            <Trash2 size={18}/>
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </>
+    )
+}
+
+export default TodoList;
